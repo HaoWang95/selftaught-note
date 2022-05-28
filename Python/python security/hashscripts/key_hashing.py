f7bbc7d5206fab259f9bd4cb625a03034ae3c9c2
HaoWang95 <alanwang0028@gmail.com>
2022-05-28 11:31:42 +1000

    update: python basic key hashing

--- Python/python security/hashscripts/key_hashing.py
+++ Python/python security/hashscripts/key_hashing.py
@@ -1,6 +1,9 @@
+import hashlib
+import json
 from secrets import token_bytes, token_hex, token_urlsafe
 import os
 from pathlib import Path
+import hmac
 
 def get_os_urandom():
     return os.urandom(64)
@@ -16,6 +19,30 @@ def get_token_urlsafe():
 
 def get_current_path():
     print(Path('.'))
+
+
+def use_hmac():
+    hmac_sha256 = hmac.new('secret_key'.encode('utf-8'), msg='thisismypassword'.encode(), digestmod=hashlib.sha256)
+    hmac_sha256.digest_size = 64
+    hashed_value = hmac_sha256.digest()
+    print(f'A hased sha256 value: {hashed_value}')
+    if hashed_value == hmac_sha256.digest():
+        print(f'Hasing the same value generates same results of length {hmac_sha256.digest_size}')
+
+    hmac_sha256.digest_size = 32
+    print(f'A 32bit hashed value: {hmac_sha256.digest()}')
+    print(f'hex version: {hmac_sha256.hexdigest()}')
+    print(hmac_sha256.name)
+
+
+def authenticate_message(msg: str='default message'):
+    hash_algo = hmac.new(b'secret_key', msg=msg.encode() ,digestmod=hashlib.sha256)
+    hash_value = hash_algo.hexdigest()
+    authenticated_message = {
+        'message': list(msg),
+        'hash_value': hash_value
+    }
+    return json.dumps(authenticated_message)
 
 
 if __name__ == '__main__':
@@ -41,3 +68,6 @@ if __name__ == '__main__':
         print(f'All three of them share the same length, {len(token_with_urlsafe.encode().hex())}')
 
     get_current_path()
+    use_hmac()
+    json_result = authenticate_message(msg='How are you doing today?')
+    print(f'hashed json message: {json_result}')
